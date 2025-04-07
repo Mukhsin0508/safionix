89734988a5db731f62a4a8028b11c52addf8243a
ogabek <ogbkiy@gmail.com>
2025-04-07 20:26:48 +0500

    xa

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,26 +1,34 @@
-import { BookingBlock, Callobaration, CommentsSection, HomeHeader, NewProjects, OfferBlock, Showcase, SkillsPart, SuccesStories } from "@/components"
+import {
+  BookingBlock,
+  
+  CommentsSection,
+  HomeHeader,
+  NewProjects,
+  OfferBlock,
+  Showcase,
+  SkillsPart,
+  SuccesStories
+} from "@/components";
+import Collaboration from "@/components/Callobaration";
 
 export default function Home() {
   return (
-    <div className="">
-      
-      <main >
-      <div className="container">
-        <OfferBlock />
-        <HomeHeader />
-        <Showcase className="mt-[110px]" />
-        <BookingBlock className="mt-[110px]" />
-        <CommentsSection className="mt-[110px]" />
-        <SkillsPart className="mt-[110px]" />
-        <SuccesStories className="mt-[110px]" />
-        <NewProjects  />
-      </div>
-       <Callobaration  />
-       <div className="container">
+    <div>
+      <main>
+        <div className="container mx-auto px-4">
+          <OfferBlock />
+          <HomeHeader />
+          <Showcase className="mt-[110px]" />
+          <BookingBlock className="mt-[110px]" />
+          <CommentsSection className="mt-[110px]" />
+          <SkillsPart className="mt-[110px]" />
+          <SuccesStories className="mt-[110px]" />
+          <NewProjects />
+          
+        </div>
 
-       </div>
+        <Collaboration  />
       </main>
-
     </div>
-  )
+  );
 }
