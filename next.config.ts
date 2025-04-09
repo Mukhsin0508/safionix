3e9c1349ac98d05268bb0fbaa3228c39c19d49e1
ogabek <ogbkiy@gmail.com>
2025-04-09 14:30:49 +0500

    fix problem calcom

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,18 @@ import createNextIntlPlugin from "next-intl/plugin"
 const withNextIntl = createNextIntlPlugin("./src/lib/i18n/request.ts")
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   /* config options here */
-  webpack: (config) => {
+  webpack: (config, {isServer}) => {
     config.module.rules.push({
       test: /\.node$/,
       use: 'node-loader',
     });
-
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
     return config;
   },
 }
