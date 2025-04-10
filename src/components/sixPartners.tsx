3cdf8f0014ca7358837c07dde0f8c4b011339b07
ogabek <ogbkiy@gmail.com>
2025-04-10 11:58:16 +0500

    hope fixed

--- src/components/sixPartners.tsx
+++ src/components/sixPartners.tsx
@@ -1,47 +1,31 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
+
 const data = [
-    {
-        name: "Tarteeb.uz",
-    },
-    {
-        name: "Speaklish.co",
-        img: "/assets/speaklishLogo.svg",
-    },
-    {
-        name: "Bonafit",
-        img: "/assets/bonafitLogo.svg",
-    },
-    {
-        name: "KRI institute",
-        img: "/assets/KriLogo.svg",
-    },
-    {
-        name: "AHA",
-        img: "/assets/AhaLogo.svg",
-    },
-    {
-        name: "Innovaite.uz",
-    },
+    { name: "Tarteeb.uz" },
+    { name: "Speaklish.co", img: "/assets/speaklishLogo.svg" },
+    { name: "Bonafit", img: "/assets/bonafitLogo.svg" },
+    { name: "KRI institute", img: "/assets/KriLogo.svg" },
+    { name: "AHA", img: "/assets/AhaLogo.svg" },
+    { name: "Innovaite.uz" },
 ];
-export default function sixPartersPage() {
-    const [activeItem, setActiveItem] = useState(null);
 
-   
+export default function SixPartnersPage() {
+    const [activeItem, setActiveItem] = useState<string | null>(null);
 
-    const handleClick = (name: any) => {
+    const handleClick = (name: string) => {
         setActiveItem(null);
         setTimeout(() => setActiveItem(name), 0);
     };
 
     return (
         <div className="flex mt-10 justify-center">
-            <div className="flex  border-dashed border-2 border-gray-200   container   items-center">
+            <div className="flex border-dashed border-2 border-gray-200 container items-center">
                 {data.map((item, index) => (
                     <div
                         key={index}
-                        className="relative w-full  flex justify-center border-dashed border-l-2  border-gray-200   px-3 py-6  items-center cursor-pointer text-lg"
+                        className="relative w-full flex justify-center border-dashed border-l-2 border-gray-200 px-3 py-6 items-center cursor-pointer text-lg"
                         onClick={() => handleClick(item.name)}
                     >
                         {item.img && (
@@ -50,7 +34,7 @@ export default function sixPartersPage() {
                                 height={40}
                                 src={item.img}
                                 alt={item.name}
-                                className=" mr-[5px] align-middle"
+                                className="mr-[5px] align-middle"
                             />
                         )}
                         <span className="text-2xl font-bold">{item.name}</span>
